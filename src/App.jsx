9981bdcee6b90f1b718e5272e85c7522ad3b05a8
Ajinkya243 <ajinkyagund2@gmail.com>
2025-12-12 12:20:50 +0530

    tailwindcss setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-
+import "tailwindcss";
+import Navbar from "./Navbar";
 function App() {
   return (
     <>
-    <h1>Initial Code</h1>
+    <h1 className="text-3xl font-bold underline bg-amber-700">Initial Code</h1>
+    <Navbar/>
       </>
   )
 }
